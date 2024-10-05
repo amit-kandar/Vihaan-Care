ac6798055309103597b30d4af7e7d01bf39e319d
Amit Kandar <kandaramit2001@gmail.com>
2024-10-06 01:43:12 +0530

    fix: add images for mobile and tablet

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,4 @@
 export const LOGO = require('../assets/removedbg_logo.png');
 export const HERO_IMG = require('../assets/cdc-VRpjDw3WqqI-unsplash.jpg')
+export const HERO_DOCTOR_MOBILE_IMG = require('../assets/premium_photo-1661764878654-3d0fc2eefcca-removebg-preview.png')
+export const HERO_DOCTOR_TABLET_IMG = require('../assets/premium_photo-1681843126728-04eab730febe-removebg-preview.png')
