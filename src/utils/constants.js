ddaef965b0f6585282d83dfe5971c1de7b959042
Amit Kandar <kandaramit2001@gmail.com>
2024-10-07 01:42:18 +0530

    feat: add a new image for desktop view

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,3 +2,4 @@ export const LOGO = require('../assets/removedbg_logo.png');
 export const HERO_IMG = require('../assets/cdc-VRpjDw3WqqI-unsplash.jpg')
 export const HERO_DOCTOR_MOBILE_IMG = require('../assets/premium_photo-1661764878654-3d0fc2eefcca-removebg-preview.png')
 export const HERO_DOCTOR_TABLET_IMG = require('../assets/premium_photo-1681843126728-04eab730febe-removebg-preview.png')
+export const HERO_DOCTOR_DESKTOP_IMG = require('../assets/premium_photo-1674841252391-efd5fe8266d5-removebg-preview.png')
